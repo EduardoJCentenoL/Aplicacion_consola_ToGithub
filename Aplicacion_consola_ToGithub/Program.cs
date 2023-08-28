dc3005747a7477edbaa493a8df155635dfdfa2d6
EduardoJCentenoL <140222534+EduardoJCentenoL@users.noreply.github.com>
2023-08-28 16:34:18 -0600

    El mensaje fue modificado de nuevo
    Desde GitHub para el mundo

--- Aplicacion_consola_ToGithub/Program.cs
+++ Aplicacion_consola_ToGithub/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("a poco sí!");
-Console.WriteLine("Otro cambio para ver que pedo");
+Console.WriteLine("Otro cambio para ver que pedo, más un cambio en GitHub");
